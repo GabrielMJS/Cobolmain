6bf722d4aa261c4bca00a7273d4754a348df6d49
Gabriel Madureira <132027963+GabrielMJS@users.noreply.github.com>
2024-12-19 18:34:18 -0300

    teste cob

---
+++ aula1.cob
@@ -0,0 +1,13 @@
+área   id DIVISION.
+livre  program-id . aula1.
+      *comentario
+       environment division.
+       
+      /OUTRA PAGINA NO RELATORIO DA COMPILAÇÃO
+       data division.
+       procedure division.
+           DISPLAY "Ola, Mundo!".
+           DISPLAY "ESTE TEXTO EH MUITO GRANDE E "
+      -    "VAI SER GERADO EM UMA LINHA NA SAIDA"
+           STOP RUN.
+           
