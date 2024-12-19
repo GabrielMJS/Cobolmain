6c0f1b0239a21697b511aeacef6ba3bd815d874b
Gabriel Madureira <gabriel.anjodemim@gmail.com>
2024-12-19 18:44:55 -0300

    inicio no cobol kkk

---
+++ test.cob
@@ -0,0 +1,12 @@
+área   id DIVISION.
+livre  program-id . test.
+      *comentario
+       environment division.
+       
+      /OUTRA PAGINA NO RELATORIO DA COMPILAÇÃO
+       data division.
+       procedure division.
+           DISPLAY "Ola, Mundo!".
+           DISPLAY "ESTE TEXTO EH MUITO GRANDE E "
+      -    "VAI SER GERADO EM UMA LINHA NA SAIDA"
+           STOP RUN.
